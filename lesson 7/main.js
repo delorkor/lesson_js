2dd98b7c5da84c38bd0c8bc814bcc734ff4638ea
PavelZubarev <213633@inbox.ru>
2023-04-26 20:10:38 +0300

    'lesson7'

---
+++ lesson 7/main.js
@@ -0,0 +1,54 @@
+function getValue() {
+    let TuduList = document.querySelector('#tudulist')
+    let Title =document.querySelector('#TitleInput')
+    let Description =document.querySelector('#descriptionInput')
+    let Submit =document.querySelector('#submit')
+    let list =document.querySelector('#list')
+
+const data =[{}]
+
+
+Submit.addEventListener('click', function (e) {
+    e.preventDefault()
+    const Val={
+        id: new Date,
+        title: Title.value,
+        description: Description.value
+        }
+    data.push(Val)
+    console.log(data)
+    list.innerHTML=''
+    for (let i = 1; i < data.length; i++) {
+     list.innerHTML+= ` <li>
+        <div class="wrapper">
+        <div id="id_title">${data[i].id}</div>
+            <h1>Title:${data[i].title}</h1>
+            <div class="description">description: :${data[i].description}</div>
+            <div class="but">
+                <button>delete</button>
+            </div>
+        </div>
+    </li>`
+        
+    }
+    TuduList.addEventListener('click', function (e) {
+    if ( e.target.textContent==='delete') {
+    let listDel=e.target.closest('li')
+    let TitleDel=e.target.closest('.wrapper').firstElementChild
+    // console.log(TitleDel.textContent)
+    listDel.remove()
+    for (let i = 1; i < data.length; i++) {
+       if (data[i].id==TitleDel.textContent) {
+        console.log(data[i] ,i)
+        data.splice(i,i)
+
+       }
+       
+    }
+}
+    })
+})
+}
+
+
+getValue()
