e8b509ad34dbc0a1ec9c6916c7deeb9c73f256b2
PavelZubarev <213633@inbox.ru>
2023-04-26 21:51:33 +0300

    'download'

--- lesson 7/main.js
+++ lesson 7/main.js
@@ -17,33 +17,46 @@ function getValue() {
       data.push(Val);
       console.log(data);
       list.innerHTML = "";
-      for (let i = 0; i < data.length; i++) {
-        list.innerHTML += ` <li>
-          <div class="wrapper">
-          <div id="id_title">${data[i].id}</div>
-              <h1>Title:${data[i].title}</h1>
-              <div class="description">description: :${data[i].description}</div>
-              <div class="but">
-                  <button>delete</button>
-              </div>
-          </div>
-      </li>`;
-      }
+      getList(data)
       TuduList.addEventListener("click", function (e) {
         if (e.target.textContent === "delete") {
-          let listDel = e.target.closest("li");
+          // let listDel = e.target.closest("li");
           let TitleDel = e.target.closest(".wrapper").firstElementChild;
           // console.log(TitleDel.textContent)
-          listDel.remove();
+          // listDel.remove();
           for (let i = 0; i < data.length; i++) {
             if (data[i].id == TitleDel.textContent) {
-              console.log(data[i], i);
+             if (i==0) {
               data.splice(i, i+1);
+             }else{ 
+              data.splice(i, i);
             }
+             
+              list.innerHTML = "";
+              getList(data)
+              console.log(data);
+            }
+            
           }
         }
       });
+      
     });
   }
   
   getValue();
+
+  function getList(data){
+    for (let i = 0; i < data.length; i++) {
+      list.innerHTML += ` <li>
+        <div class="wrapper">
+        <div id="id_title">${data[i].id}</div>
+            <h1>Title:${data[i].title}</h1>
+            <div class="description">description: :${data[i].description}</div>
+            <div class="but">
+                <button>delete</button>
+            </div>
+        </div>
+    </li>`;
+    }
+  }
